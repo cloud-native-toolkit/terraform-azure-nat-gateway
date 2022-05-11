f772ded2cba90aec3b216a407844e0aacbcd5c9a
Sudhir Jain <sudhir.jain@ibm.com>
2022-05-11 10:43:44 +0530

    2 create azure nat gateway (#3)
    * Created terraform module for Azure NAT Gateway.  Unit Test pending
    
    * Update main.tf
    
    * added unit test files and. module.yaml file udpated
    
    * updated variable.tf file
    
    * formatted main.tf files
    
    * Workflow updated to current (#4)
    
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
    
    * Created terraform module for Azure NAT Gateway.  Unit Test pending
    
    * Update main.tf
    
    * added unit test files and. module.yaml file udpated
    
    * updated variable.tf file
    
    * formatted main.tf files
    
    * Updated variable.tf file with line number 62 as empty
    
    * Signed-off-by: Sudhir Jain <sudhir.jain@ibm.com>
    
    Co-Authored-By: hemang-ibm <103431190+hemang-ibm@users.noreply.github.com>
    
    Co-authored-by: hemang-ibm <103431190+hemang-ibm@users.noreply.github.com>
    Co-authored-by: Rich Ehrhardt <51330294+rich-ehrhardt@users.noreply.github.com>
    Co-authored-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

---
+++ main.tf
@@ -0,0 +1,39 @@
+
+locals {
+  name_public_ip        = "${var.nat_gw_name}-ip"
+  name_public_ip_prefix = "${var.nat_gw_name}-ipPrefix"
+}
+resource "azurerm_public_ip" "public_ip" {
+  name                = var.public_ip_name == "" ? local.name_public_ip : var.public_ip_name
+  location            = var.region
+  resource_group_name = var.resource_group_name
+  allocation_method   = var.public_ip_allocation_method
+  sku                 = var.public_ip_sku
+  zones               = var.public_ip_zones
+}
+
+resource "azurerm_public_ip_prefix" "public_ip_prefix" {
+  name                = var.public_ip_prefix_name == "" ? local.name_public_ip_prefix : var.public_ip_prefix_name
+  location            = var.region
+  resource_group_name = var.resource_group_name
+  prefix_length       = var.public_ip_prefix_length
+  zones               = var.public_ip_zones
+}
+
+resource "azurerm_nat_gateway" "nat_gateway" {
+  name                    = var.nat_gw_name
+  location                = var.region
+  resource_group_name     = var.resource_group_name
+  sku_name                = var.nat_sku_name
+  idle_timeout_in_minutes = var.nat_idle_timeout
+  zones                   = var.nat_zones
+}
+resource "azurerm_nat_gateway_public_ip_association" "nat_gateway_public_ip_association" {
+  nat_gateway_id       = azurerm_nat_gateway.nat_gateway.id
+  public_ip_address_id = azurerm_public_ip.public_ip.id
+}
+
+resource "azurerm_nat_gateway_public_ip_prefix_association" "nat_gateway_public_ip_prefix_association" {
+  nat_gateway_id      = azurerm_nat_gateway.nat_gateway.id
+  public_ip_prefix_id = azurerm_public_ip_prefix.public_ip_prefix.id
+}
--- variables.tf
+++ variables.tf
@@ -1,6 +1,65 @@
-#variable "myvariable" {
-#  type        = string
-#  description = "A description of my variable"
-#  default     = ""
-#}
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the Azure resource group where the NAT gateway has been provisioned"
+}
+
+variable "region" {
+  type        = string
+  description = "The Azure location where the NAT gateway will be installed"
+}
+variable "nat_gw_name" {
+  type        = string
+  description = "The name of Azure NAT Gateway which will be created"
+}
+variable "public_ip_name" {
+  type        = string
+  description = "A Public IP Address id which should be associated with the NAT Gateway resource."
+  default     = ""
+}
+variable "public_ip_prefix_name" {
+  type        = string
+  description = "The name of public IP prefix which should be associated with the NAT Gateway resource."
+  default     = ""
+}
+variable "public_ip_allocation_method" {
+  type        = string
+  description = "Public IP address allocation method - Static or Dynamic"
+  default     = "Static"
+}
+variable "public_ip_sku" {
+  type        = string
+  description = "The SKU which should be used. At this time the only supported value is Standard. Defaults to Standard."
+  default     = "Standard"
+}
+variable "public_ip_zones" {
+  type        = list(string)
+  description = "Specifies a list of Availability Zones in which this NAT Gateway should be located."
+  default     = ["1"]
+}
+variable "public_ip_prefix_length" {
+  type        = number
+  description = "The public IP prefix length"
+  default     = 30
+}
+variable "nat_sku_name" {
+  type        = string
+  description = "The NAT SKU name"
+  default     = "Standard"
+}
+variable "nat_idle_timeout" {
+  type        = number
+  description = "The idle timeout which should be used in minutes."
+  default     = 10
+}
+variable "nat_zones" {
+  type        = list(string)
+  description = "Specifies a list of Availability Zones in which this NAT Gateway should be located. Changing this forces a new NAT Gateway to be created"
+  default     = ["1"]
+}
+variable "tags" {
+  type        = list(string)
+  description = "A mapping of tags to assign to the resource. Changing this forces a new resource to be created."
+  default     = []
+}
+
 
