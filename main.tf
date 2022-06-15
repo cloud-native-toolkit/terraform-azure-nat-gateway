c86cd991bd0a5b8bc077a50e9597c38e709304a7
Saurabh Rajguru <saurabh.rajguru@gmail.com>
2022-06-15 08:33:57 +0530

    Add ability to use existing public ip address (#6)
    * Added ability to use existing public ip
    
    Added ability to use existing public ip.
    
    Signed-off-by: Saurabh Rajguru <saurabh.rajguru@ibm.com>
    
    * Removed unused block
    
    Removed unused block
    
    Signed-off-by: Saurabh Rajguru <saurabh.rajguru@ibm.com>

--- main.tf
+++ main.tf
@@ -1,15 +1,21 @@
-
 locals {
   name_public_ip        = "${var.nat_gw_name}-ip"
   name_public_ip_prefix = "${var.nat_gw_name}-ipPrefix"
 }
 resource "azurerm_public_ip" "public_ip" {
+  count               = var.existing_public_ip_name == "" ? 1 : 0
   name                = var.public_ip_name == "" ? local.name_public_ip : var.public_ip_name
   location            = var.region
   resource_group_name = var.resource_group_name
   allocation_method   = var.public_ip_allocation_method
   sku                 = var.public_ip_sku
   zones               = var.public_ip_zones
+}
+
+data "azurerm_public_ip" "public_ip" {
+  count               = var.existing_public_ip_name == "" ? 0 : 1
+  name                = var.existing_public_ip_name
+  resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_public_ip_prefix" "public_ip_prefix" {
@@ -30,7 +36,7 @@ resource "azurerm_nat_gateway" "nat_gateway" {
 }
 resource "azurerm_nat_gateway_public_ip_association" "nat_gateway_public_ip_association" {
   nat_gateway_id       = azurerm_nat_gateway.nat_gateway.id
-  public_ip_address_id = azurerm_public_ip.public_ip.id
+  public_ip_address_id = var.existing_public_ip_name == "" ? azurerm_public_ip.public_ip[0].id : data.azurerm_public_ip.public_ip[0].id
 }
 
 resource "azurerm_nat_gateway_public_ip_prefix_association" "nat_gateway_public_ip_prefix_association" {
