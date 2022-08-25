8eed90e74f287353876d7d8b509162ebe6e8a007
Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
2022-08-26 09:59:53 +1000

    Modified public ip handling and added public ip outputs
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

--- main.tf
+++ main.tf
@@ -2,8 +2,10 @@ locals {
   name_public_ip        = "${var.nat_gw_name}-ip"
   name_public_ip_prefix = "${var.nat_gw_name}-ipPrefix"
 }
+
 resource "azurerm_public_ip" "public_ip" {
   count               = var.existing_public_ip_name == "" ? 1 : 0
+
   name                = var.public_ip_name == "" ? local.name_public_ip : var.public_ip_name
   location            = var.region
   resource_group_name = var.resource_group_name
@@ -13,8 +15,7 @@ resource "azurerm_public_ip" "public_ip" {
 }
 
 data "azurerm_public_ip" "public_ip" {
-  count               = var.existing_public_ip_name == "" ? 0 : 1
-  name                = var.existing_public_ip_name
+  name                = var.existing_public_ip_name == "" ? var.existing_public_ip_name : azurerm_public_ip.public_ip[0].name
   resource_group_name = var.resource_group_name
 }
 
@@ -36,7 +37,7 @@ resource "azurerm_nat_gateway" "nat_gateway" {
 }
 resource "azurerm_nat_gateway_public_ip_association" "nat_gateway_public_ip_association" {
   nat_gateway_id       = azurerm_nat_gateway.nat_gateway.id
-  public_ip_address_id = var.existing_public_ip_name == "" ? azurerm_public_ip.public_ip[0].id : data.azurerm_public_ip.public_ip[0].id
+  public_ip_address_id = data.azurerm_public_ip.public_ip.id
 }
 
 resource "azurerm_nat_gateway_public_ip_prefix_association" "nat_gateway_public_ip_prefix_association" {
