73780c9afe1ba8d61952d7502d19f0acf840971d
Rich Ehrhardt <51330294+rich-ehrhardt@users.noreply.github.com>
2022-08-26 10:12:40 +1000

    8 use random naming for GitHub testing to avoid conflicts with other testing (#10)
    * Updated test stage to use random name
    
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
    
    * Removed boilerplate readme contents
    
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
    
    * Added input and output variables to readme
    
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
    
    * Added output to module metadata
    
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
    
    * Modified public ip handling and added public ip outputs
    
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
    
    * Fixed order of public ip data source name conditional
    
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
    
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,18 @@ output "id" {
   value       = azurerm_nat_gateway.nat_gateway.id
   depends_on  = [azurerm_nat_gateway.nat_gateway]
 }
+
+output "ip_address" {
+  description = "The address of the public IP created or supplied"
+  value = data.azurerm_public_ip.public_ip.ip_address
+}
+
+output "public_ip_id" {
+  description = "The Azure Id of the public IP"
+  value = data.azurerm_public_ip.public_ip.id
+}
+
+output "fqdn" {
+  description = "The FQDN of the A DNS record associated with the public IP"
+  value = data.azurerm_public_ip.public_ip.fqdn
+}
