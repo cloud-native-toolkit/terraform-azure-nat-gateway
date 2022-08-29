d3817456b3e2a4d54edbe1429dbc1a20260fa559
rich-ehrhardt <rich_ehrhardt@au1.ibm.com>
2022-08-29 16:01:12 +1000

    Added test stages for subnet association
    Signed-off-by: rich-ehrhardt <rich_ehrhardt@au1.ibm.com>

--- test/stages/stage2-nat-gateway.tf
+++ test/stages/stage2-nat-gateway.tf
@@ -1,6 +1,8 @@
 module "azure-nat_gateway" {
   source               = "./module"
+  
   resource_group_name  = module.resource_group.name
-  region               = var.region
-  nat_gw_name          = "nat_gw_name"
+  region               = module.resource_group.region
+  nat_gw_name          = "${local.name_prefix}-nat"
+  subnet_id            = module.subnets.id
 }
--- test/stages/variables.tf
+++ test/stages/variables.tf
@@ -3,6 +3,18 @@ variable "resource_group_name" {
   description = "Resource group to be created."
 }
 
+variable "name_prefix" {
+  type        = string
+  description = "Prefix name that should be used for the cluster and services. If not provided then resource_group_name will be used"
+  default     = ""
+}
+
+variable "vnet_cidr" {
+  type = string
+  description = "CIDR for VNet"
+  default = "10.0.0.0/18"
+}
+
 variable "region" {
   type        = string
   description = "Region/location to deploy into."
